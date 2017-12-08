198b3f8627ac2f2537f3e786a32218fb420ae9d4
Michal Hricisin <hricisin@bart.sk>
2017-12-08 19:05:03 +0100

    misova hracka... cmdline

--- src/test.js
+++ src/test.js
@@ -1,13 +1,44 @@
+import optimist from 'optimist';
+import prompt from 'prompt';
 import API from './services/ApiService';
 
-(async () => {
+prompt.start();
+
+var schema = {
+  properties: {
+    username: {
+      default: 'Kotkodaky_8'
+    },
+    password: {
+      default: 'kikirik'
+    }
+  }
+};
+
+prompt.get(schema, async function (err, result) {
   try {
-    await API.join('Kotkodaky_8', 'kikirik');
+    await API.join(result.username, result.password);
     const myPos = await API.getMyPosition();
     console.log(myPos);
     // const move = await API.move({x: 10, y: 2});
     // console.log(move);
+    if (optimist.argv.m === 'up') {
+      const move = await API.move({x: myPos.x, y: myPos.y - 1});
+      console.log(move);
+    }
+    if (optimist.argv.m === 'down') {
+      const move = await API.move({x: myPos.x, y: myPos.y + 1});
+      console.log(move);
+    }
+    if (optimist.argv.m === 'left') {
+      const move = await API.move({x: myPos.x - 1, y: myPos.y});
+      console.log(move);
+    }
+    if (optimist.argv.m === 'right') {
+      const move = await API.move({x: myPos.x + 1, y: myPos.y});
+      console.log(move);
+    }
   } catch (e) {
     console.log(e);
   }
-})();
+});
