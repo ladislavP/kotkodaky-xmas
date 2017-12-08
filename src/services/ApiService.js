095a4c0556016188ba07e52c4067f0ec427f8a72
Ladislav Perduk <ladislav.perduk@gmail.com>
2017-12-08 18:10:28 +0100

    Zaklady

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,5 +1,15 @@
 class ApiService {
 
+    constructor(){
+        this.api_url = "http://bciapi.eu-west-1.elasticbeanstalk.com/game/";
+    }
 
+    async getBoard(){
+        let response = await fetch(this.api_url + 'board',{
+            headers: {
+                'Authorization': 'Basic '+btoa('Kotkodaky_8:kikirik'),
+            }
+        })
+    }
 
 }
