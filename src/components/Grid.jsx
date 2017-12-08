5c5ec8b970951c8f666a511183f5a819acb36dba
Ladislav Perduk <ladislav.perduk@gmail.com>
2017-12-08 17:57:16 +0100

    Pridanie componentov

---
+++ src/components/Grid.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Grid extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="grid">
+
+            </div>
+        );
+    }
+}
+
+Grid.propTypes = {};
+
+export default Grid;
---
+++ src/components/Man.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Man extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Man.propTypes = {};
+
+export default Man;
